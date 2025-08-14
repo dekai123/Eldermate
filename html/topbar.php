e1e90d8fe0206ba45b949ca912fab4bc412ced36
dekai123 <dekaiapichad@gmail.com>
2025-08-14 11:14:09 +0700

    Add files via upload

---
+++ html/topbar.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+     <link rel="stylesheet" href="style.css">
+</head>
+<body>
+        <div class="top-bar">
+        <div class="top-bar-tel">
+            <p>02-221-1501</p>
+        </div>
+        <div class="top-bar-shop">
+            <p>Welcome to Eldermate</p>
+        </div>
+    </div>
+    <nav>
+        <div class="logo"><!--รูปโลโก้-->
+            <a href="#">Eldermate<img src="" alt=""></a>
+        </div>
+        <ul class="menu"><!--เมนูต่างๆ-->
+            <li><a href="index.html">HOME</a></li>
+            <li><a href="about us.html">ABOUT US</a></li>
+            </ul>
+        <div class="search-box"> 
+            <i class="fa fa-search"></i>
+            <input type="text" class="search-input" placeholder="S E A R C H">
+            <i class="fa fa-microphone"></i>
+        </div> 
+
+        <ul class="menu2">
+            <li><a href="question.html">QUESTION</a></li>
+            <li><a href="contact us.html">CONTACT US</a></li>
+            </ul>
+        <div id="result">
+          </div>
+    </nav>
+</body>
+</html>
